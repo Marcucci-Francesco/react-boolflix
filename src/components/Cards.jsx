7db79572979162199be2c54fff3ab429d03dd84c
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-01-29 13:55:23 +0100

    feat: add milestone 4

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,6 @@
 
 import { useGlobalContext } from "../context/GlobalContext"
+import placeholder from '../../public/placeholder.jpeg'
 
 
 const Card = ({ film, serie }) => {
@@ -23,27 +24,53 @@ const Card = ({ film, serie }) => {
     return voteRating
   }
 
-  function flagsImg() {
-    return film ? (flags(film?.original_language) || `https://flagsapi.com/${film?.original_language.toUpperCase()}/shiny/64.png`) : (flags(serie?.original_language || `https://flagsapi.com/${serie?.original_language.toUpperCase()}/shiny/64.png`))
+  const posterImg = () => {
+    const posterPath = film ? film?.poster_path : serie?.poster_path;
+
+    if (posterPath) {
+      return `https://image.tmdb.org/t/p/w342/${posterPath}`
+    } else {
+      return placeholder
+    }
   }
 
-  function posterImg() {
-    return film ? `https://image.tmdb.org/t/p/w342/${film?.poster_path}` : `https://image.tmdb.org/t/p/w342/${serie?.poster_path}`
+  function overview() {
+    const overviewText = film?.overview || serie?.overview
+
+    return overviewText || 'Not available anymore';
   }
+
+  function stars() {
+    return getStars(film?.vote_average) || getStars(serie?.vote_average)
+  }
+
+  function flagsImg() {
+    const language = film ? film?.original_language : serie?.original_language;
+    const flagUrl = flags(language) || `https://flagsapi.com/${language.toUpperCase()}/shiny/64.png`;
+
+    if (flags(language)) {
+      return flagUrl;
+    } else {
+      return language.toUpperCase();
+    }
+  }
+
 
   return (
     <>
-      <div className="card col-3 my-4 mx-4">
-        <div className="mt-2">
-          {film?.title || serie?.name}
-        </div>
-        <ul className="list-group my-4">
-          <li className="list-group-item">{film?.original_title || serie?.original_name}</li>
-          <li className="list-group-item">
-            <img src={flagsImg()} alt={film?.original_language || serie?.original_language} /></li>
-          <li className="list-group-item">{getStars(film?.vote_average) || getStars(serie?.vote_average)}</li>
+      <div className="card col-3 my-4 mx-4 bg-black text-white">
+        <img className="poster img-fluid" src={posterImg()} />
+        <ul className="card-body my-4 overflow-y-auto">
+          <div className="card-title mb-2">
+            {`Title: ${film?.title || serie?.name}`}
+          </div>
+          <li className="card-subtitle">{`Original Title: ${film ? (film?.original_title) : (serie?.original_name)}`}</li>
+          <li className="card-text">
+            {flagsImg().includes("http") ? (<img src={flagsImg()} alt={film?.original_language || serie?.original_language} />) : (<span>{`Language: ${flagsImg()}`}</span>)}
+          </li>
+          <li className="card-text  text-warning">{stars()}</li>
+          <li className="card-text">{`Overview: ${overview()}`}</li>
         </ul>
-        <img src={posterImg()} />
       </div>
     </>
   )
